28202135c11ef793daa5c49f9500f4028636fbc4
Lauren Riddle <lari7132@gmail.com>
2019-10-29 13:02:18 -0500

    got the journal entries to POST to API

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,27 +1,28 @@
-// const date = document.getElementById("date").value
-// const subject = document.getElementById("subject").value
-// const entry = document.getElementById("entry").value
-// const mood = document.getElementsByClassName("mood").value
+const date = document.getElementById("date").value
+const subject = document.getElementById("subject").value
+const entry = document.getElementById("entry").value
+const mood = document.getElementsByClassName("mood").value
 
 const API = {
-  date: document.getElementById("date").value,
-  subject: document.getElementById("subject").value,
-  entry: document.getElementById("entry").value,
-  mood: document.getElementsByClassName("mood").value,
   getJournalEntries() {
     return fetch("http://localhost:3000/entries")
       .then(response => response.json())
   },
-  entryObject: (date, subject, entry, mood) => {
-    return {
+  // entryObject: (date, subject, entry, mood) => {
+  //   return {
+  //     "date": date,
+  //     "concept": subject,
+  //     "entry": entry,
+  //     "mood": mood
+  //   }
+  // },
+  saveJournalEntry: (entryObject) => {
+    const newJournalEntry = {
       "date": date,
       "concept": subject,
       "entry": entry,
       "mood": mood
     }
-  },
-  saveJournalEntry: (entryObject) => {
-    const newJournalEntry = this.entryObject(this.date, this.subject, this.entry, this.mood)
     fetch("http://localhost:3000/entries", { // Replace "url" with your API's URL
       method: "POST",
       headers: {
@@ -50,7 +51,7 @@ export default API
 //   },
 //   addEntryToAPI: () => {
 //     // Invoke the factory function, passing along the form field values
-//     const newJournalEntry = this.createEntryObject()
+//     const newJournalEntry = createEntryObject()
 
 //     // Use `fetch` with the POST method to add your entry to your API
 //     fetch("http://localhost:3000/entries", { // Replace "url" with your API's URL
