10c3d6f4d8b14dbc0ff4b44ba0b06d9eb5713d04
Lauren Riddle <lari7132@gmail.com>
2019-10-30 13:26:18 -0500

    refactored the code to POST the new entry to the database and reload the data on the page

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,38 +1,17 @@
 
-import renderDom from "./entriesDOM.js"
-
 const API = {
   getJournalEntries() {
     return fetch("http://localhost:3000/entries")
       .then(response => response.json())
   },
-  saveJournalEntry() {
-    const date = document.getElementById("date").value
-    const subject = document.getElementById("subject").value
-    const entry = document.getElementById("entry").value
-    const mood = document.getElementById("mood").value
-    if (date === "" || subject === "" || entry === "") {
-      alert("Required fields must be filled out.")
-    } if (subject.length >= 30) {
-      alert("Concepts field length is too long. Please shorten message.")
-    } else {
-      let newJournalEntry = {
-        "date": date,
-        "concept": subject,
-        "entry": entry,
-        "mood": mood
-      }
-      fetch("http://localhost:3000/entries", { // Replace "url" with your API's URL
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newJournalEntry)
-      })
-      return fetch("http://localhost:3000/entries")
-      .then(response => response.json())
-      .then(response => renderDom.renderJournalEntries(response))
-    }
+  postObject(entry) {
+    return fetch("http://localhost:3000/entries", { // Replace "url" with your API's URL
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(entry)
+    })
   }
 }
 export default API
