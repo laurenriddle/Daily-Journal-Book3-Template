68659773703ad3d67b061f83f38cefe22b33b212
Lauren Riddle <lari7132@gmail.com>
2019-10-31 15:31:53 -0500

    added event listener and function to delete journal entries

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -17,13 +17,28 @@ htmlElements.buildAndAppendFilterElement()
 API.getJournalEntries()
     .then(response => renderDom.renderJournalEntries(response))
 
-const button = document.querySelector("button")
-button.addEventListener("click", saveObject.postEntry)
+    const logArticle = document.querySelector(".entryLog")
+
+    logArticle.addEventListener("click", event => {
+        if (event.target.id.startsWith("deleteButton--")) {
+            // Extract recipe id from the button's id attribute
+            const entryToDelete = event.target.id.split("--")[1]
+
+            // Invoke the delete method, then get all recipes and render them
+            API.deleteSingleJournalEntry(entryToDelete)
+                .then(API.getJournalEntries)
+                .then(response => renderDom.renderJournalEntries(response))
+            }
+    })
+
+
+
+
+const saveJournalEntryButton = document.querySelector("button")
+saveJournalEntryButton.addEventListener("click", saveObject.postEntry)
 
 
 const radioButtons = document.getElementsByName("moodButton")
-
-
 radioButtons.forEach((button) => {
     button.addEventListener("click", event => {
         const mood = event.target.value
@@ -31,21 +46,18 @@ radioButtons.forEach((button) => {
 
         API.getJournalEntries()
             .then(response => {
-                let filteredArray =
-
-                    response.filter(entry => {
-                        let moodBoolean = false
-                        // console.log(entry.mood)
-                        if (entry.mood === mood) {
-                            moodBoolean = true
-                            console.log("this is working!", mood)
-                        } else if (mood === "All") {
-                            moodBoolean = true
-                        }
-                        return moodBoolean
-                        // console.log(moodBoolean)
-                    })
-
+                let filteredArray = response.filter(entry => {
+                    let moodBoolean = false
+                    // console.log(entry.mood)
+                    if (entry.mood === mood) {
+                        moodBoolean = true
+                        // console.log("this is working!", mood)
+                    } else if (mood === "All") {
+                        moodBoolean = true
+                    }
+                    return moodBoolean
+                    // console.log(moodBoolean)
+                })
 
                 renderDom.renderJournalEntries(filteredArray)
 
